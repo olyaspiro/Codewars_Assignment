71a19c035f52526c17f44cb4ee515a5ad3125f0b
Olha Spiro <olichkaprimak@gmail.com>
2025-05-15 16:38:25 -0500

    commit message

---
+++ codewars.py
@@ -0,0 +1,18 @@
+# Even or Odd
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+    
+
+# Convert a Number to a String 
+def number_to_string(num):
+    return str(num)
+
+
+
+# Remove String Spaces
+def no_space(x):
+    return "".join(x.split())
+
